c771f51ba6a563fc5f79bc12e44a956aa6bf8dd9
Robert Jenssen <jenssen.90@gmail.com>
2018-12-04 21:49:17 +0100

    Changed port number

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const helmet = require('helmet')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.use(bodyParser.json())
 app.use(helmet())
